f7729ab6beb6546cfb9f33af957ab69a6519b086
Nathaniel Case <Qalthos@gmail.com>
2020-06-04 13:13:46 -0400

    More feedback when running

--- run.py
+++ run.py
@@ -5,10 +5,14 @@ from mailer import send_mail
 
 if __name__ == "__main__":
     triager = Triager()
+    print("Gathering issues for triage...")
     issues = triager.triage()
 
     if issues:
         table = make_table(issues)
+        print(table)
+
+        print("Mailing table to maintainers...")
         send_mail(
             content=table,
             sender=triager.sender,
